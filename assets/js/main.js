b3648146127674c60c44f783e4cbb02910d9a2e4
Mona Baroud <your-email-address>
2018-10-10 12:19:03 +0200

    1
    1

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+	/*$("p").click (function(){
+       $(this).hide();
+       //$('p').hide();
+	});
+	// body...*/
+	/*$('p').hover(function(){
+		alert("Hello");
+	},
+	function () {
+		// body...
+	alert("bye");
+	});*/
+ 
+
+$('input').focus(function(){
+	$(this).css("background-color","red");
+});
+//blur
+
+
+});
